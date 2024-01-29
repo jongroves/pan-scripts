c69f2375c19938f13c836753ddfeda18a7ac55aa
Jonathan Groves <joncgroves@gmail.com>
2024-01-29 07:36:22 -0600

    added support for creating directories that do not exist

--- pan-backup/pan-backup-bulk.sh
+++ pan-backup/pan-backup-bulk.sh
@@ -8,15 +8,24 @@ TAGS=$(cat .panrc | grep hostname | awk -F % '{print $2}' | awk -F = '{print $1}
 #OR set TAGS to custom text file containing list of specific .panrc tag names
 #TAGS=$(cat firewalls.txt)
 
-#Global variables:
-FILE_DIR=/path/to/backup/folder
+#Path to main backup folder. A subfolder per tag will be created inside this directory if they do not exist.
+BACKUP_DIR=/path/to/backup/folder
 
 for TAG in $TAGS; do
+
+    #Path to tag-specific backup folder.
+    TAG_DIR=$BACKUP_DIR/$TAG
+
+    #Check to see if $TAG directory exists. If not, create it.
+    if [ ! -d "$TAG_DIR" ]; then
+        echo "Directory $TAG_DIR does not exist. Creating..."
+        mkdir "$TAG_DIR"
+    fi
 
     echo "Getting device-state export for tag $TAG"
 
     #exported file parameters
-    FILENAME=$TAG/device_state_cfg-$(date +"%Y%m%d-%H%M%S").tgz
+    FILENAME=device_state_cfg-$(date +"%Y%m%d-%H%M%S").tgz
 
     #PAN-OS device state export
     panxapi.py -t $TAG --export device-state --dst $FILE_DIR/$FILENAME
